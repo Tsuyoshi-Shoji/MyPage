dd2960924a621977385edb2302869e3749853d32
USER <user@USERnoMacBook-ea.local>
2026-03-05 23:50:46 +0900

    レスポンシブ対応(モバイル端末用のcss設定)

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,27 @@
 import styles from "@/styles/header.module.css"
 import Link from 'next/link'
 import Nav from "@/components/nav"
+import { useState } from 'react'
 
 export default function Header() {
+  const [isTouched, setIsTouched] = useState(false)
+
+  const handleTouchStart = () => {
+    setIsTouched(true)
+  }
+
+  const handleTouchEnd = () => {
+    setIsTouched(false)
+  }
+
   return (
     <header className={styles.header}>
-      <Link href="/" legacyBehavior   >
-        <div className={styles.logo}>
+      <Link href="/" legacyBehavior>
+        <div 
+          className={`${styles.logo} ${isTouched ? styles.touched : ''}`}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+        >
           <span className={styles.portfolio}>Portfolio</span>
           <span className={styles.createdBy}> created by </span>
           <span className={styles.name}>Tsuyoshi Shoji</span>
